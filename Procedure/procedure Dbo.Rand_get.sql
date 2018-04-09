b46a754053e2c78ffb00101470e5325ff25b5f80
NatalyTim <nataly.sun89@mail.ru>
2018-04-09 23:12:45 +0300

    Format code

--- Procedure/procedure Dbo.Rand_get.sql
+++ Procedure/procedure Dbo.Rand_get.sql
@@ -2,17 +2,17 @@ if(object_id('Dbo.Rand_get')is not null)
 drop procedure Dbo.Rand_get
 go
 create procedure Dbo.Rand_get(
- @IntFrom integer
-,@intTo integer
-,@QWE integer out
+     @IntFrom integer
+    ,@intTo integer
+    ,@QWE integer out
 )
 as 
 begin
-Select @QWE= convert(integer, rand()* (@IntFrom-@intTo)+@intTo)
-return
+    Select @QWE= convert(integer, rand()* (@IntFrom-@intTo)+@intTo)
+    return
 end
 go
-
+/*
 Declare @A integer
  exec Dbo.Rand_get
  @IntFrom =3
@@ -20,3 +20,4 @@ Declare @A integer
 ,@QWE =@A out
 
 select @a
+*/
